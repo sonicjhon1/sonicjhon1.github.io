5ed74e1a3d13f5f3075c6ea5608070c0395d2cd5
sonicjhon1 <sonicjhon56@gmail.com>
2022-09-23 08:27:10 +0700

    [ASSET] Add webp and script to convert to webp

---
+++ assets/images/GFX/bulk-convert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Copy this file to any directory containing images & then run this script in bash shell
+# Get all png images in the current directory & convert it to webp format
+for file in *.png ;
+do
+cwebp -q 80 "$file" -o "${file%.png}.webp";
+done
