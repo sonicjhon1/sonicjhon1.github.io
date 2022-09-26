b373c500c4f0039361208fa97bed86c27455db1d
sonicjhon1 <sonicjhon56@gmail.com>
2022-09-26 11:52:38 +0700

    [SH] Fix bug

--- assets/images/WEB/bulk-convert.sh
+++ assets/images/WEB/bulk-convert.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Copy this file to any directory containing images & then run this script in bash shell
 # Get all png images in the current directory & convert it to webp format
+q=65;
+
 for file in *.png ;
 do
     if [ "${file}" = "*.png" ];
     then echo "No png";
     else
-        read w h < <(identify -format "%w %h" $file)
+        echo "${file}"
+        read w h < <(identify -format "%w %h" "${file}")
         echo $w,$h
         n=$w
-        [ $h -lt $n ] && n=$h && cwebp -mt -m 6 -resize 0 $((n/2)) -q 80 "${file}" -o "${file%.png}tmp.webp";
-        [ $h -gt $n ] && cwebp -mt -m 6 -resize $((n/2)) 0 -q 80 "${file}" -o "${file%.png}tmp.webp";
+        [ $h -eq $n ] && cwebp -mt -m 6 -resize $((n/2)) $((n/2)) -q "${q}" "${file}" -o "${file%.png}tmp.webp";
+        [ $h -lt $n ] && n=$h && cwebp -mt -m 6 -resize 0 $((n/2)) -q "${q}" "${file}" -o "${file%.png}tmp.webp";
+        [ $h -gt $n ] && n=$w && cwebp -mt -m 6 -resize $((n/2)) 0 -q "${q}" "${file}" -o "${file%.png}tmp.webp";
         n=$((n/2))
         convert "${file%.png}tmp.webp" -gravity center -extent "${n}x${n}" "${file%.png}.webp";
         rm "${file%.png}tmp.webp";
@@ -25,8 +29,9 @@ do
         read w h < <(identify -format "%w %h" $file)
         echo $w,$h
         n=$w
-        [ $h -lt $n ] && n=$h && cwebp -mt -m 6 -resize 0 $((n/2)) -q 80 "${file}" -o "${file%.jpg}tmp.webp";
-        [ $h -gt $n ] && cwebp -mt -m 6 -resize $((n/2)) 0 -q 80 "${file}" -o "${file%.jpg}tmp.webp";
+        [ $h -eq $n ] && cwebp -mt -m 6 -resize $((n/2)) $((n/2)) -q "${q}" "${file}" -o "${file%.png}tmp.webp";
+        [ $h -lt $n ] && n=$h && cwebp -mt -m 6 -resize 0 $((n/2)) -q "${q}" "${file}" -o "${file%.jpg}tmp.webp";
+        [ $h -gt $n ] && n=$w && cwebp -mt -m 6 -resize $((n/2)) 0 -q "${q}" "${file}" -o "${file%.jpg}tmp.webp";
         n=$((n/2))
         convert "${file%.jpg}tmp.webp" -gravity center -extent "${n}x${n}" "${file%.jpg}.webp";
         rm "${file%.jpg}tmp.webp";
