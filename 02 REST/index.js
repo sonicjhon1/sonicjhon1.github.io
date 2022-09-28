5c773308717345f3688284ad44432539cbfd9bb8
sonicjhon1 <sonicjhon56@gmail.com>
2022-09-28 11:16:47 +0700

    [WORKER] Simple working

--- 02 REST/index.js
+++ 02 REST/index.js
@@ -1,12 +1,8 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  var result = await fetch("https://sonicj.pages.dev/assets/images/1.webp")
+  return result
 }
