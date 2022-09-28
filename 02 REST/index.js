cdac3e4c20fbea1604f6bc85093cb590230f806a
sonicjhon1 <sonicjhon56@gmail.com>
2022-09-28 13:51:58 +0700

    [WORKER] Working on instagram

--- 02 REST/index.js
+++ 02 REST/index.js
@@ -1,8 +1,28 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+/**
+ * An object with different URLs to fetch
+ * @param {Object} ORIGINS
+ */
+ const ORIGINS = {
+  'starwarsapi.yourdomain.com': 'swapi.dev',
+  'google.yourdomain.com': 'www.google.com',
+};
 
-async function handleRequest(request) {
-  var result = await fetch("https://sonicj.pages.dev/assets/images/1.webp")
-  return result
+function handleRequest(request) {
+  const url = new URL(request.url);
+  // Check if incoming hostname is a key in the ORIGINS object
+  if (true) {
+    const target = "i.instagram.com"
+    const ptarget = "api/v1/users/web_profile_info/?username=sonic_jhon"
+    url.hostname = target;
+    url.pathname = ptarget;
+    // If it is, proxy request to that third party origin
+    return fetch(url.toString(), request);
+  }
+
+  // Otherwise, process request as normal
+  return fetch(request);
 }
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
