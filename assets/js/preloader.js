9003b8c72ce97e2d5b15add1bb18e35f37d62ae8
sonicjhon1 <sonicjhon56@gmail.com>
2022-09-30 10:09:14 +0700

    [HTML, JS] Remove unused javascript code

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,23 +1,3 @@
-var wrapper = document.querySelector(".gaspar");
-var buttons = document.querySelectorAll(".style-switch-wrapper .cursor li a");
-var shows = document.querySelectorAll(".style-switch-wrapper .cursor li a.show");
-var hides = document.querySelectorAll(".style-switch-wrapper .cursor li a.hide");
-
-buttons.forEach(button => {
-  button.on("click", function () {
-    var nav_target = document.querySelector(this);
-    return ( nav_target.classList.contains("showme") || (button.classList.remove("showme"), nav_target.classList.add("showme")), false );
-  });
-});
-
-shows.forEach(show => {
-  show.on("click", wrapper.setAttribute("data-magic-cursor", ""));
-});
-
-hides.forEach(hide => {
-  hide.on("click", wrapper.setAttribute("data-magic-cursor", "hide"));
-});
-
 docReady( function() {
   var preloader = document.querySelector("#preloader");
   sleep(1000).then(() => {preloader.classList.add("preloaded")});
