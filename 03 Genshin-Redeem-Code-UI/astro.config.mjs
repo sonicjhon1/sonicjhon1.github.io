5b1dfb3d0a5bbc9874a8b9f6671e15b2640d2c04
sonicjhon1 <sonicjhon56@gmail.com>
2022-10-07 17:09:19 +0700

    [ASTRO] Base

--- 03 Genshin-Redeem-Code-UI/astro.config.mjs
+++ 03 Genshin-Redeem-Code-UI/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sonicjhon1.github.io',
+  base: '/03 Genshin-Redeem-Code-UI',
   integrations: [sitemap(), tailwind()],
   output: 'static',
   server: { port: 3000, host: true}
