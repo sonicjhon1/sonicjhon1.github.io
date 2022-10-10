c222f0ddc3b7cd97f65a94358130e36afaa8e661
sonicjhon1 <sonicjhon56@gmail.com>
2022-10-10 14:33:27 +0700

    [ASTRO] Change site

--- 03 Genshin-Redeem-Code-UI/astro.config.mjs
+++ 03 Genshin-Redeem-Code-UI/astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sonicjhon1.github.io',
+  site: 'https://sonicj.pages.dev',
   base: '/03 Genshin-Redeem-Code-UI',
   integrations: [sitemap(), tailwind()],
   output: 'static',
