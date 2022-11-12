48b5937a193b361ae8b20a3b04d405fa8e820080
sonicjhon1 <sonicjhon56@gmail.com>
2022-11-12 15:15:31 +0700

    [MISC] Retry?

--- 03 Genshin-Redeem-Code-UI/build.sh
+++ 03 Genshin-Redeem-Code-UI/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Setup NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
 # Setup NodeJS
 nvm install node
