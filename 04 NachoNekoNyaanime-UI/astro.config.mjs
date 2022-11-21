7430e96cf4fe3e6c1bb660d19b9f822cb4180b6e
sonicjhon1 <sonicjhon56@gmail.com>
2022-11-21 14:56:39 +0700

    [ASTRO] Fix broken builds

--- 04 NachoNekoNyaanime-UI/astro.config.mjs
+++ 04 NachoNekoNyaanime-UI/astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sonicj.pages.dev',
+  base: '/04 NachoNekoNyaanime-UI',
   build: {
     // Generate `page.html` instead of `page/index.html` during build.
     format: 'file'
