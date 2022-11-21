6b3d4a1f5014944d65ee5de307f0a59b7da36ee3
sonicjhon1 <sonicjhon56@gmail.com>
2022-11-21 14:48:09 +0700

    [SH] Update build script

---
+++ 04 NachoNekoNyaanime-UI/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Setup NVM
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
+# Setup NodeJS
+# nvm install node && nvm use node
+
+# Install NPM deps
+npm install
+
+# Build and deploy the page
+npm run build
+mv dist/* .
+rmdir dist
