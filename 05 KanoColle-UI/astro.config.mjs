0074772f770e58091e25065a988ffac1ce0ef7e0
sonicjhon1 <sonicjhon56@gmail.com>
2022-11-25 08:34:10 +0700

    [ASSET, ASTRO] Init 05

---
+++ 05 KanoColle-UI/astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sonicj.pages.dev',
+  base: '/05 KanoColle-UI',
+  build: {
+    // Generate `page.html` instead of `page/index.html` during build.
+    format: 'file'
+  },
+  integrations: [],
+  output: 'static',
+  server: {
+    port: 3000,
+    host: true
+  }
+});
