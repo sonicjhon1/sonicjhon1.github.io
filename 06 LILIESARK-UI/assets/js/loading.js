de9b78cc62487511bddc96070e9a95e394c761ab
sonicjhon1 <sonicjhon56@gmail.com>
2022-11-29 14:56:04 +0700

    [JS] Barebone loading script

--- 06 LILIESARK-UI/assets/js/loading.js
+++ 06 LILIESARK-UI/assets/js/loading.js
@@ -1,3 +1,24 @@
-function showLoading() {
-    
+// Init variables
+var loader;
+
+// Fill in variables
+async function initLoader() {
+    loader = document.querySelector('#loading');
 }
+
+// Event handlers
+async function attachLoader() {
+    await initLoader();
+}
+
+function showLoading() {
+    audioMenuBGM('stop');
+    
+    loader.classList.toggle('hide');
+    loader.classList.toggle('fadeOut');
+    loader.classList.toggle('fadeIn');
+}
+
+// On ready
+docReady(initLoader);
+docReady(attachLoader);
