cb7c81e53e2ad77407001d84296b62f7e11ac0f5
sonicjhon1 <sonicjhon56@gmail.com>
2022-11-29 16:28:11 +0700

    [CSS, HTML, JS] Implement loading screen

--- 06 LILIESARK-UI/assets/js/loading.js
+++ 06 LILIESARK-UI/assets/js/loading.js
@@ -6,19 +6,25 @@ async function initLoader() {
     loader = document.querySelector('#loading');
 }
 
-// Event handlers.
-async function attachLoader() {
-    await initLoader();
-}
-
-function showLoading() {
+async function showLoading() {
     audioMenuBGM('stop');
 
     loader.classList.toggle('hide');
+    loader.classList.toggle('fadeOut');
+    loader.classList.toggle('fadeIn');
+
+    await sleep(6000)
+    hideLoading();
+    await sleep(1000)
+    loader.classList.toggle('hide');
+
+    //startTrailer();
+}
+
+function hideLoading() {
     loader.classList.toggle('fadeOut');
     loader.classList.toggle('fadeIn');
 }
 
 // On ready.
 docReady(initLoader);
-docReady(attachLoader);
