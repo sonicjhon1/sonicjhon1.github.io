16bd0c79763292d48910d159151bcf5dae05efd3
sonicjhon1 <sonicjhon56@gmail.com>
2022-12-09 12:54:51 +0700

    [SH] Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "03 Genshin-Redeem-Code-UI" && chmod +x build.sh && ./build.sh && cd ..
-cd "04 NachoNekoNyaanime-UI" && chmod +x build.sh && ./build.sh
+cd "04 NachoNekoNyaanime-UI" && chmod +x build.sh && ./build.sh && cd ..
