e6450dc598ae38841207afe4aab4e0d6e943c617
sonicjhon1 <sonicjhon56@gmail.com>
2022-12-16 09:21:53 +0700

    [ASTRO, NPM] Update Astro, Add astro-compress

--- 05 KanoColle-UI/astro.config.mjs
+++ 05 KanoColle-UI/astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sonicj.pages.dev',
-  base: '/05 KanoColle-UI',
+  base: '.',
   build: {
     // Generate `page.html` instead of `page/index.html` during build.
     format: 'file'
   },
-  integrations: [],
+  integrations: [compress()],
   output: 'static',
   server: {
     port: 3000,
