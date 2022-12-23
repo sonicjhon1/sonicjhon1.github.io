d3aaa21c1114129dfd5dc790263d139e55683829
sonicjhon1 <sonicjhon56@gmail.com>
2022-12-23 13:52:01 +0700

    [ANDROID] Move into 1 folder, setup live-reload

--- 08 GT-Reminder-UI/astro.config.mjs
+++ 08 GT-Reminder-UI/astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://sonicj.pages.dev',
+    build: {
+        // Generate `page.html` instead of `page/index.html` during build.
+        format: 'file'
+    },
+    integrations: [],
+    output: 'static',
+    server: {
+        port: 3000,
+        host: true
+    }
+});
