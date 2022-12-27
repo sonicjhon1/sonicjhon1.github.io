8f61d3d37f9a49bd92eb7708a6007b2d3f053787
sonicjhon1 <sonicjhon56@gmail.com>
2022-12-27 09:21:43 +0700

    [CF] A hacky but working JWT

--- 08 GT-Reminder/index.js
+++ 08 GT-Reminder/index.js
@@ -1,43 +1,43 @@
-import auth from './auth0-jwt-validation'
+addEventListener("fetch", event => {
+	event.respondWith(handleRequest(event.request));
+});
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-}) 
+async function parseJwt(str) {
+	if (str.startsWith("eyJ")) {
+		return true;
+	}
+	return false;
+}
 
-/**
- * Fetch and log a request
- * @param {Request} request
- */
 async function handleRequest(request) {
-  try {
-    let isValid = await auth.isValidJwt(request)
-    if (!isValid) {
-      return new Response("false", { 
-        headers: {
-          "Access-Control-Allow-Origin":"*",
-          "Access-Control-Allow-Headers": "*"
-        },
-        status: 200
-      })
-    }
-  
-    console.log('JWT is valid')
-    return new Response("true", { 
-      headers: {
-        "Access-Control-Allow-Origin":"*",
-        "Access-Control-Allow-Headers": "*"
-      },
-      status: 200
-    })
-  } 
-  catch (error) {
-    console.log(error)
-    return new Response("false", { 
-      headers: {
-        "Access-Control-Allow-Origin":"*",
-        "Access-Control-Allow-Headers": "*"
-      },
-      status: 200
-    })
-  }
+	const jwt = request.headers.get("Authorization");
+	const result = await parseJwt(jwt);
+
+	try {
+		if (result) {
+			return new Response("false", {
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Headers": "*"
+				},
+				status: 404
+			});
+		} else {
+			return new Response("true", {
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Headers": "*"
+				},
+				status: 200
+			});
+		}
+	} catch (error) {
+		return new Response("error", {
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Headers": "*"
+			},
+			status: 404
+		});
+	}
 }
