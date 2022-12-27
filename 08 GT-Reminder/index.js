9fa7a80def34eaf935da67110de449e7738f939f
sonicjhon1 <sonicjhon56@gmail.com>
2022-12-27 10:17:11 +0700

    [ASTRO, CF] A working JWT token checker!

--- 08 GT-Reminder/index.js
+++ 08 GT-Reminder/index.js
@@ -1,7 +1,3 @@
-addEventListener("fetch", event => {
-	event.respondWith(handleRequest(event.request));
-});
-
 async function parseJwt(str) {
   if (str == null && str == undefined) {
     return false;
@@ -11,34 +7,41 @@ async function parseJwt(str) {
   }
 }
 
+const header = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "*"
+}
+
+addEventListener("fetch", event => {
+	event.respondWith(handleRequest(event.request));
+});
+
+
 async function handleRequest(request) {
+  if (request.method !== 'GET') {
+    return new Response(null, {
+      headers: header,
+      status: 200
+    });
+  }
+
 	const jwt = request.headers.get("Authorization");
 	const result = await parseJwt(jwt);
-
 	try {
 		if (!result) {
 			return new Response("false", {
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Headers": "*"
-				},
+				headers: header,
 				status: 404
 			});
 		} else {
 			return new Response("true", {
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Headers": "*"
-				},
+				headers: header,
 				status: 200
 			});
 		}
 	} catch (error) {
 		return new Response("error", {
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Headers": "*"
-			},
+			headers: header,
 			status: 404
 		});
 	}
