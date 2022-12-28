5f7443feeee53a9ac0354b9d98ac7828b0039315
sonicjhon1 <sonicjhon56@gmail.com>
2022-12-28 11:21:45 +0700

    [ASTRO] Add barebone homepage styled with Tailwind

--- 08 GT-Reminder-UI/astro.config.mjs
+++ 08 GT-Reminder-UI/astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sonicj.pages.dev',
-    build: {
-        // Generate `page.html` instead of `page/index.html` during build.
-        format: 'file'
-    },
-    integrations: [],
-    output: 'static',
-    server: {
-        port: 3000,
-        host: true
-    }
+  site: 'https://sonicj.pages.dev',
+  build: {
+    // Generate `page.html` instead of `page/index.html` during build.
+    format: 'file'
+  },
+  integrations: [tailwind(), compress()],
+  output: 'static',
+  server: {
+    port: 3000,
+    host: true
+  }
 });
