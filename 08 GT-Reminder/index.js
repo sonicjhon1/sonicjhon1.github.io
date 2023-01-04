2ed5e4c737c8a818f04109168d0fa4372f67163f
sonicjhon1 <sonicjhon56@gmail.com>
2023-01-04 12:35:26 +0700

    [CF] Tried implementing license checking

--- 08 GT-Reminder/index.js
+++ 08 GT-Reminder/index.js
@@ -1,3 +1,5 @@
+const secretKey = "SonicjPassword";
+
 async function parseJwt(str) {
 	if (str == null && str == undefined) {
 		return false;
@@ -27,33 +29,13 @@ async function handleRequest(request) {
 	const url = new URL(request.url);
 	const path = url.pathname;
 	if (request.method == "GET") {
-		if (path !== "/verify") {
-			const jwt = request.headers.get("Authorization");
-			const result = await parseJwt(jwt);
-			try {
-				if (!result) {
-					return new Response("false", {
-						headers: header,
-						status: 404
-					});
-				} else {
-					return new Response("true", {
-						headers: header,
-						status: 200
-					});
-				}
-			} catch (error) {
-				return new Response("error", {
-					headers: header,
-					status: 404
-				});
-			}
+		if (path === "/encrypt") {
 		}
 
 		if (path === "/verify") {
 			const jwt = request.headers.get("Authorization");
 			const result = await parseJwt(jwt);
-			
+
 			if (!result) {
 				return new Response(null, {
 					headers: header,
@@ -63,9 +45,9 @@ async function handleRequest(request) {
 
 			const jsonData = JSON.parse(request.headers.get("data"));
 			authToken = jsonData[0];
-			userInfoJSON = jsonData[1]
-			userLicenseKey = jsonData[2]
-			dateString = jsonData[3]
+			userEmail = jsonData[1].email;
+			userLicenseKey = jsonData[2];
+			dateString = jsonData[3];
 
 			if (jwt !== authToken) {
 				return new Response(null, {
@@ -73,13 +55,34 @@ async function handleRequest(request) {
 					status: 200
 				});
 			}
-			
-			const data = '{"1": ' + request.headers.get("data") + '}';
+
+			const data = '{"1": ' + request.headers.get("data") + "}";
 			response = data;
 
 			return new Response(response, {
 				headers: header,
 				status: 200
+			});
+		}
+
+		const jwt = request.headers.get("Authorization");
+		const result = await parseJwt(jwt);
+		try {
+			if (!result) {
+				return new Response("false", {
+					headers: header,
+					status: 404
+				});
+			} else {
+				return new Response("true", {
+					headers: header,
+					status: 200
+				});
+			}
+		} catch (error) {
+			return new Response("error", {
+				headers: header,
+				status: 404
 			});
 		}
 	}
