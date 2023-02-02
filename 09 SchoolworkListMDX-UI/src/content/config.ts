edbb2dac125955b70a27a7e66e7f6e5495d1dd05
Sonicj <sonicjhon56@gmail.com>
2023-02-02 11:42:51 +0700

    [ASTRO, MDX] Make use of ContentCollection

---
+++ 09 SchoolworkListMDX-UI/src/content/config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// 2. Define a schema for each collection you'd like to validate.
+const xicmiaCollection = defineCollection({
+	schema: z.object({
+        judul: z.string(),
+        tanggal: z.string().transform(str => new Date(str)),
+        mataPelajaran: z.enum([
+                "#KIMIA", "#FISIKA", "#BIO", "#MATEMINAT", // Minat di MIA
+                "#MATEWAJIB", "#SEJARAHWAJIB", // Wajib di MIA
+                "#BINDO", "#ENG", "#MAND", // Bahasa
+                "#SBK", "#AGAMA", "#PPKN", "#TIK", "#PJOK" // Lainnya
+        ]),
+        dalam: z.string(),
+	}),
+});
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+	xicmia: xicmiaCollection,
+};
