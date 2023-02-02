fd20ff75ef09b1a7eb38031a7deec8d4dba20700
Sonicj <sonicjhon56@gmail.com>
2023-02-02 15:06:10 +0700

    [ASTRO, MDX] Seperate example to its own group

--- 09 SchoolworkListMDX-UI/src/content/config.ts
+++ 09 SchoolworkListMDX-UI/src/content/config.ts
@@ -1,22 +1,42 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 // 2. Define a schema for each collection you'd like to validate.
+const exampleCollection = defineCollection({
+	schema: z.object({
+		judul: z.string(),
+		tanggalKasih: z.string().transform((str) => new Date(str)),
+		tanggalKumpul: z.string().transform((str) => new Date(str)),
+		mataPelajaran: z.enum([
+			"#EXAMPLE", // Example
+		]),
+		dalam: z.string(),
+	}),
+});
 const xicmiaCollection = defineCollection({
 	schema: z.object({
-        judul: z.string(),
-        tanggalKasih: z.string().transform(str => new Date(str)),
-        tanggalKumpul: z.string().transform(str => new Date(str)),
-        mataPelajaran: z.enum([
-                "#KIMIA", "#FISIKA", "#BIO", "#MATEMINAT", // Minat di MIA
-                "#MATEWAJIB", "#SEJARAHWAJIB", // Wajib di MIA
-                "#BINDO", "#ENG", "#MAND", // Bahasa
-                "#SBK", "#AGAMA", "#PPKN", "#TIK", "#PJOK", // Lainnya
-                "#EXAMPLE", // Example
-        ]),
-        dalam: z.string(),
+		judul: z.string(),
+		tanggalKasih: z.string().transform((str) => new Date(str)),
+		tanggalKumpul: z.string().transform((str) => new Date(str)),
+		mataPelajaran: z.enum([
+			"#KIMIA",
+			"#FISIKA",
+			"#BIO",
+			"#MATEMINAT", // Minat di MIA
+			"#MATEWAJIB",
+			"#SEJARAHWAJIB", // Wajib di MIA
+			"#BINDO",
+			"#ENG",
+			"#MAND", // Bahasa
+			"#SBK",
+			"#AGAMA",
+			"#PPKN",
+			"#TIK",
+			"#PJOK", // Lainnya
+		]),
+		dalam: z.string(),
 	}),
 });
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-	xicmia: xicmiaCollection,
+	example: exampleCollection, xicmia: xicmiaCollection, 
 };
