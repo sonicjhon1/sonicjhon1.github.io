07af6d06a405c58ca18f1f0145c3cf4852ae1dc6
Sonicj <sonicjhon56@gmail.com>
2023-02-02 10:28:47 +0700

    [ASTRO] Add workaround Options for Meta tags compression

--- 09 SchoolworkList-UI/astro.config.mjs
+++ 09 SchoolworkList-UI/astro.config.mjs
@@ -5,13 +5,18 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
 export default defineConfig({
-  site: 'https://sonicj.pages.dev',
-  base: '/09 SchoolworkList-UI',
+  site: 'https://sonicj.pages.dev/',
+  base: '09 SchoolworkList-UI',
   build: {
     // Generate `page.html` instead of `page/index.html` during build.
     format: 'file'
   },
-  integrations: [tailwind(), compress({html: false})],
+  integrations: [tailwind(), compress({
+    html: {
+      removeAttributeQuotes: false,
+      decodeEntities: true,
+    }
+  })],
   output: 'static',
   server: {
     port: 3000,
