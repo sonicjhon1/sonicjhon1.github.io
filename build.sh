b807b40da2a1ed5fe2fc48d67e95205eaa3712f7
Sonicj <sonicjhon56@gmail.com>
2023-02-04 12:49:14 +0700

    [SH] Update and simplify build scripts

--- build.sh
+++ build.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
 
-# Install NPM deps
-npm install
+# Build commands
+prebuild() {
+    # Install NPM deps
+    npm install
+}
+build() {
+    # Launch build scripts
+    chmod +x build.sh
+    ./build.sh
+}
+postBuild() {
+    # Copy all the content of "dist" and removes the "dist" and "node_modules" folder
+    cp -R dist/* .
+    rm -rfd node_modules
+    rm -rfd dist
+}
 
-# Build and deploy the page
-npm run build
-cp -R dist/* .
-rm -rfd node_modules
-rm -rfd dist
+# Initiate build process and deploy the page
+prebuild
+npx turbo run build
+postBuild
 
 # Build folders 
 build03UI() {
     cd "03 Genshin-Redeem-Code-UI"
-    chmod +x build.sh
-    ./build.sh
+    postBuild
 }
 build04UI() {
     cd "04 NachoNekoNyaanime-UI"
-    chmod +x build.sh
-    ./build.sh
+    postBuild
 }
 build05UI() {
     cd "05 KanoColle-UI"
-    chmod +x build.sh
-    ./build.sh
+    postBuild
 }
 build09UI() {
     cd "09 SchoolworkList-UI"
-    chmod +x build.sh
-    ./build.sh
+    postBuild
 }
 build09MDXUI() {
     cd "09 SchoolworkListMDX-UI"
-    chmod +x build.sh
-    ./build.sh
+    postBuild
 }
 
 build03UI & build04UI & build05UI & build09UI & build09MDXUI
-echo "Done!"
