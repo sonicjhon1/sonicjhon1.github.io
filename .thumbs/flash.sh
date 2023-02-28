aafef32fc0f25a3a0367c4153c8577bdd6c62276
Sonicj <sonicjhon56@gmail.com>
2023-02-28 13:35:21 +0700

    [SH] Auto space encode

--- .thumbs/flash.sh
+++ .thumbs/flash.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-projects=("01%20Redirect-UI" "03%20Genshin-Redeem-Code-UI" "04%20NachoNekoNyaanime-UI" "05%20KanoColle-UI" "06%20LILIESARK-UI" "07%20BelajarBersama-UI" "08%20GT-Reminder-UI" "09%20SchoolworkList-UI")
+projects=("01 Redirect-UI" "03 Genshin-Redeem-Code-UI" "04 NachoNekoNyaanime-UI" "05 KanoColle-UI" "06 LILIESARK-UI" "07 BelajarBersama-UI" "08 GT-Reminder-UI" "09 SchoolworkList-UI")
 IFS=""
 
 for project in ${projects[@]}; do
-    curl "https://api.apiflash.com/v1/urltoimage?access_key=79b0e7c367b84a8387c8041566437096&url=https://sonicj.pages.dev/${project}&format=webp&response_type=image&width=1280&height=720&quality=90" -o "${project}.webp"
+    projectHTML="${project// /%20}"
+    curl "https://api.apiflash.com/v1/urltoimage?access_key=79b0e7c367b84a8387c8041566437096&url=https://sonicj.pages.dev/${projectHTML}&format=webp&response_type=image&width=1280&height=720&quality=90" -o "${project}.webp"
 done
