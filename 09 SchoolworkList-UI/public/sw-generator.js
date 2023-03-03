657c1191ccf2044c8b9e784dab3f7c4e52396bc8
Sonicj <sonicjhon56@gmail.com>
2023-03-03 13:04:46 +0700

    [ASTRO, NPM] Add potential offline page, add auto generating SW

--- 09 SchoolworkList-UI/public/sw-generator.js
+++ 09 SchoolworkList-UI/public/sw-generator.js
@@ -1,21 +1,26 @@
 import workbox from "workbox-build";
 
 workbox.generateSW({
-    cacheId: "SWL",
-    globDirectory: "./",
-    globPatterns: ["**/*.{css, js, webp, png, jpg}"],
-    globIgnores: ["**/ServiceWorker.*", "**/swl-service-worker.js", "**node_modules/**/*"],
-    swDest: "public/swl-service-worker.js",
-    runtimeCaching: [
-        {
-            urlPattern: /\.(?:html|htm|xml)$/,
-            handler: "StaleWhileRevalidate",
-            options: {
-                cacheName: "swlmarkup",
-                expiration: {
-                    maxAgeSeconds: 1000
+	cacheId: "SWL",
+	globDirectory: "./",
+	globPatterns: ["**/*.{css, js, webp, png, jpg}"],
+	globIgnores: ["**/ServiceWorker.*", "**/swl-service-worker.js", "**node_modules/**/*"],
+	swDest: "public/swl-service-worker.js",
+	runtimeCaching: [
+		{
+			urlPattern: /\.(?:html|htm|xml)$/,
+			handler: "StaleWhileRevalidate",
+			options: {
+				cacheName: "swlmarkup",
+				expiration: {
+					maxAgeSeconds: 1000,
+				},
+                precacheFallback: {
+                    fallbackURL: './index.html',
                 }
-            }
-        }
-    ]
-})
+			},
+		},
+	],
+}).then(({count, size}) => {
+    console.log(`Generated service worker, which will precache ${count} files, totaling ${size} bytes.`);
+}).catch(console.error);
