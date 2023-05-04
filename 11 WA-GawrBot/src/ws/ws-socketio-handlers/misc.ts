79157ff1593a9e9055ca1068ccddd81748b0384c
Sonicj <sonicjhon56@gmail.com>
2023-05-04 18:11:54 +0700

    [TS] Listen for disconnect events

--- 11 WA-GawrBot/src/ws/ws-socketio-handlers/misc.ts
+++ 11 WA-GawrBot/src/ws/ws-socketio-handlers/misc.ts
@@ -4,7 +4,10 @@ export default function handleWebSocket(io: Server) {
 	let listening = false;
 
 	const connection = async (socket: Socket) => {
-		console.info(`socket ${socket.id} connected`);
+		console.info(`socket ${socket.id} connected.`);
+		socket.on("disconnect", (reason) => {
+			console.info(`socket ${socket.id} disconnected because of ${reason}.`);
+		});
 	};
 
 	const listen = () => {
