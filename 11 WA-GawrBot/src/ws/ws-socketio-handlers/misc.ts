5448918ddeda1a2e8212fc55058f3c92628f595e
Sonicj <sonicjhon56@gmail.com>
2023-05-04 18:00:50 +0700

    [TS] More better naming

--- 11 WA-GawrBot/src/ws/ws-socketio-handlers/misc.ts
+++ 11 WA-GawrBot/src/ws/ws-socketio-handlers/misc.ts
@@ -3,21 +3,21 @@ import type { Server, Socket } from "socket.io";
 export default function handleWebSocket(io: Server) {
 	let listening = false;
 
-	const set = async (socket: Socket) => {
-		console.log(`socket ${socket.id} connected`);
+	const connection = async (socket: Socket) => {
+		console.info(`socket ${socket.id} connected`);
 	};
 
 	const listen = () => {
 		if (listening) return;
 
-		io.on("connection", set);
+		io.on("connection", connection);
 		listening = true;
 	};
 
 	const unlisten = () => {
 		if (!listening) return;
 
-		io.off("connection", set);
+		io.off("connection", connection);
 		listening = false;
 	};
 
