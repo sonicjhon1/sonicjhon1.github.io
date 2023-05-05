1c0c35991af13bebc423030ae0c29800cbae5d1a
Sonicj <sonicjhon56@gmail.com>
2023-05-05 13:09:02 +0700

    [ENV] Env typing

--- 11 WA-GawrBot-UI/src/env.d.ts
+++ 11 WA-GawrBot-UI/src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="astro/client-image" />
+interface ImportMetaEnv {
+	readonly PUBLIC_SOCKET_SERVER_URL: string | null | undefined;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
