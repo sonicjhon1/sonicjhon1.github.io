6a92318834f8a86d3493159d8490f3107f6474be
Sonicj <sonicjhon56@gmail.com>
2023-05-05 11:40:22 +0700

    [ASSET, ASTRO, PNPM] Init 11-UI

---
+++ 11 WA-GawrBot-UI/astro.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import node from "@astrojs/node";
+
+const SITE = "https://sonicj.pages.dev/";
+const BASE = import.meta.env.PROD ? "11 WA-GawrBot-UI" : "/";
+
+export default defineConfig({
+	site: SITE,
+	base: BASE,
+	build: {
+		// Generate `page.html` instead of `page/index.html` during build.
+		format: "file",
+	},
+	integrations: [
+		tailwind(),
+    compress({
+			html: {
+				removeAttributeQuotes: false,
+				decodeEntities: true,
+			},
+		}),
+	],
+	experimental: {
+		assets: true,
+	},
+	output: "server",
+	server: {
+		port: 3006,
+		host: true,
+	},
+	adapter: node({
+		mode: "standalone",
+	}),
+});
