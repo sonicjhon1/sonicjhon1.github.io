7ada9aac4453af2deb93d7f26a122993f79ce764
Sonicj <sonicjhon56@gmail.com>
2023-05-09 15:00:11 +0700

    [ASTRO] Add custom font

--- 11 WA-GawrBot-UI/tailwind.config.cjs
+++ 11 WA-GawrBot-UI/tailwind.config.cjs
@@ -2,10 +2,38 @@
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				roboto: ["Roboto", "sans-serif"],
+			},
+		},
 	},
 	plugins: [require("daisyui")],
 	daisyui: {
-		themes: ["light", "dark", "emerald", "corporate", "cyberpunk", "valentine", "halloween", "garden", "forest", "lofi", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+		themes: [
+			"light",
+			"dark",
+			"emerald",
+			"corporate",
+			"cyberpunk",
+			"valentine",
+			"halloween",
+			"garden",
+			"forest",
+			"lofi",
+			"fantasy",
+			"wireframe",
+			"black",
+			"luxury",
+			"dracula",
+			"cmyk",
+			"autumn",
+			"business",
+			"acid",
+			"lemonade",
+			"night",
+			"coffee",
+			"winter",
+		],
 	},
 };
