affcb0c462cca4e8a20370c98dbef6f256cc16b0
Sonicj <sonicjhon56@gmail.com>
2023-05-10 20:59:14 +0700

    [ASTRO, TS] Implement image util, and cache optimized images

---
+++ 11 WA-GawrBot-UI/src/assets/ts/util/image.ts
@@ -0,0 +1,28 @@
+import sharp from "sharp";
+import { cache } from "./imageCache";
+const nullObj = {};
+
+export async function optimizeImage(src: string, format: keyof sharp.FormatEnum, width: number, height: number): Promise<string> {
+	const metadata = `${src}-${width}.${height}-${format}`;
+	const map = cache.get(nullObj) ?? new Map<string, string>();
+
+	// Check if the Base64 image is already cached for the current request
+	const cachedImage = map.get(metadata);
+	if (cachedImage) {
+		return cachedImage;
+	}
+
+	// Generate the Base64 image using sharp and store it in the cache
+	const buffer = await sharp(await (await fetch(src)).arrayBuffer())
+		.resize({
+			width,
+			height,
+			fit: sharp.fit.cover,
+		})
+		.toFormat(format)
+		.toBuffer();
+	const base64 = `data:image/${format};base64,${buffer.toString("base64")}`;
+	map.set(metadata, base64);
+	cache.set(nullObj, map);
+	return base64;
+}
---
+++ 11 WA-GawrBot-UI/src/assets/ts/util/imageCache.ts
@@ -0,0 +1,1 @@
+export const cache = new WeakMap<object, Map<string, string>>();
